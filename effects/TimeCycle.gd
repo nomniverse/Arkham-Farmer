d62362bae6d850a559179c7a68bf7c34a4ebc473
Nam Tran <omn0mn0m@protonmail.com>
2020-11-26 02:40:31 -0500

    Added game files

---
+++ effects/TimeCycle.gd
@@ -0,0 +1,141 @@
+extends CanvasModulate
+
+
+signal day_changed(day)
+signal season_changed(season)
+signal time_changed(time)
+
+
+# Declare member variables here. Examples:
+export(float) var day_real_time = 180 # Time in seconds for one full day
+
+# Modulate colours
+export(Color) var dawn_color = Color(0.86, 0.70, 0.70, 1.0)
+export(Color) var day_color = Color(1.0, 1.0, 1.0, 1.0)
+export(Color) var dusk_color = Color(0.59, 0.66, 0.78, 1.0)
+export(Color) var night_color = Color(0.07, 0.09, 0.28, 1.0)
+
+# Modulate times
+export(int) var dawn_hour = 5
+export(int) var day_hour = 7
+export(int) var dusk_hour = 17
+export(int) var night_hour = 19
+
+# Time variables
+var time_ticks = 0
+var time = {
+	"hour": 0,
+	"minute": 0
+}
+
+var ticks_per_day
+var transition_duration
+
+var current_cycle
+enum Cycle {
+	NIGHT,
+	DAWN,
+	DAY,
+	DUSK,
+}
+
+# Calendar variables
+var day = 0
+
+enum Season {
+	SPRING,
+	SUMMER,
+	FALL,
+	WINTER
+}
+
+var season = Season.SPRING
+
+
+# Modulate transition properties
+export(float) var state_transition_duration = 1 # Time in-game hours between colours
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ticks_per_day = day_real_time / 24
+	transition_duration = (ticks_per_day * state_transition_duration) / 60
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
+	_handle_time()
+	_handle_date()
+	
+	time_ticks += 1
+	
+
+func _handle_time():
+	time['hour'] = int(time_ticks / ticks_per_day)
+	time['minute'] = int(fmod(time_ticks, ticks_per_day) / ticks_per_day * 60)
+	
+	if time_ticks >= day_real_time:
+		time_ticks = 0
+		
+	emit_signal("time_changed", time)
+		
+	if time['hour'] >= night_hour or time['hour'] < dawn_hour:
+		if current_cycle != Cycle.NIGHT:
+			current_cycle = Cycle.NIGHT
+		
+			$Tween.interpolate_property(self, "color", dusk_color, night_color, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.start()
+	elif time['hour'] >= dawn_hour and time['hour'] < day_hour:
+		if current_cycle != Cycle.DAWN:
+			current_cycle = Cycle.DAWN
+			
+			$Tween.interpolate_property(self, "color", night_color, dawn_color, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.start()
+	elif time['hour'] >= day_hour and time['hour'] < dusk_hour:
+		if current_cycle != Cycle.DAY:
+			current_cycle = Cycle.DAY
+			
+			$Tween.interpolate_property(self, "color", dawn_color, day_color, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.start()
+	elif time['hour'] >= dusk_hour and time['hour'] < night_hour:
+		if current_cycle != Cycle.DUSK:
+			current_cycle = Cycle.DUSK
+			
+			$Tween.interpolate_property(self, "color", day_color, dusk_color, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.start()
+
+
+func _handle_date():
+	# Change the season
+	if day == 31:
+		day = 0
+		
+		if season == Season.WINTER:
+			season = Season.SPRING
+		else:
+			season += 1
+			
+		emit_signal("season_changed", get_season())
+		
+	# Change the day
+	if time_ticks == 0:
+		day += 1
+		
+		emit_signal("day_changed", day)
+
+
+func get_season():
+	if season == Season.SPRING:
+		return "Spring"
+	elif season == Season.SUMMER:
+		return "Summer"
+	elif season == Season.FALL:
+		return "Fall"
+	elif season == Season.WINTER:
+		return "Winter"
+	else:
+		return ""
---
+++ gui/Hotbar.gd
@@ -0,0 +1,144 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+var active_slot = 0
+
+
+func get_active_slot_item():
+	return get_child(active_slot).item
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_child(active_slot).set_active()
+	
+	# Preloads inventory with items
+	# TODO Read inventory from save file
+	add_item(Items.Item.WATER_CAN)
+	add_item(Items.Item.SHOVEL)
+	add_item(Items.Item.AXE)
+	add_item(Items.Item.CORN_SEEDS)
+	add_item(Items.Item.LOG)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				get_child(active_slot).set_inactive()
+				
+				if active_slot == 9:
+					active_slot = 0
+				else:
+					active_slot += 1
+					
+				get_child(active_slot).set_active()
+				
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				get_child(active_slot).set_inactive()
+				
+				if active_slot == 0:
+					active_slot = 9
+				else:
+					active_slot -= 1
+				
+				get_child(active_slot).set_active()
+	
+	if event is InputEventKey:
+		if event.pressed:
+			_check_for_hotbar_key(event.scancode)
+
+
+func add_item(id):
+	var empty_slot = -1
+	
+	var i = 0
+	
+	for child in get_children():
+		if child.get_item()['name'] == "" and empty_slot == -1:
+			empty_slot = i
+		elif child.get_item()['name'] == Items.item_properties[id]['name']:
+			child.set_quantity(child.get_quantity() + 1)
+			return
+			
+		i += 1
+		
+	if empty_slot != -1:
+		get_children()[empty_slot].set_item(id)
+	else:
+		print("No empty slot found... Item lost.")
+	
+	
+func remove_item(item):
+	for child in get_children():
+		if child.get_item()['name'] == item['name']:
+			child.set_quantity(child.get_quantity() - 1)
+			return
+			
+	print("Item not found...")
+	
+	
+func empty_slot(number):
+	get_child(number).empty()
+	
+	
+func empty_active_slot():
+	empty_slot(active_slot)
+
+
+func _check_for_hotbar_key(scancode):
+	if scancode == KEY_1:
+		get_child(active_slot).set_inactive()
+		active_slot = 0
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_2:
+		get_child(active_slot).set_inactive()
+		active_slot = 1
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_3:
+		get_child(active_slot).set_inactive()
+		active_slot = 2
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_4:
+		get_child(active_slot).set_inactive()
+		active_slot = 3
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_5:
+		get_child(active_slot).set_inactive()
+		active_slot = 4
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_6:
+		get_child(active_slot).set_inactive()
+		active_slot = 5
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_7:
+		get_child(active_slot).set_inactive()
+		active_slot = 6
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_8:
+		get_child(active_slot).set_inactive()
+		active_slot = 7
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_9:
+		get_child(active_slot).set_inactive()
+		active_slot = 8
+		get_child(active_slot).set_active()
+		
+	if scancode == KEY_0:
+		get_child(active_slot).set_inactive()
+		active_slot = 9
+		get_child(active_slot).set_active()
