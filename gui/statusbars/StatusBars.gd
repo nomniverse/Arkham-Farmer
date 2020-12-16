88a1ce264653ae6113cb33e0ea8f3dbf15a17f51
Nam Tran <omn0mn0m@protonmail.com>
2020-12-16 03:43:26 -0500

    Added working status bar

---
+++ gui/statusbars/StatusBars.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+class_name StatusBars
+
+
+# Declare member variables here. Examples:
+onready var health_bar = $HealthBar
+onready var stamina_bar = $StaminaBar
+onready var fear_bar = $FearBar
